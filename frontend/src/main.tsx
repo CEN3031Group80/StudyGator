e3fe17c2a0fe390e59f2a3b6229ef771945d8d25
Chase MacDonnell <macdonnell.chase@gmail.com>
2024-03-27 15:12:09 -0400

    Include credentials

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,12 +6,14 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 let client = new ApolloClient({
   uri: 'https://studygator-api.chasemacdonnell.net/query',
   cache: new InMemoryCache(),
+  credentials: "include"
 });
 
 if (import.meta.env.DEV) {
   client = new ApolloClient({
     uri: '/api/query',
     cache: new InMemoryCache(),
+    credentials: "include"
   });
 }
 
