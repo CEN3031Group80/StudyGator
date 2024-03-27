029169fd44db55490c9f1fe851ce36d2aff4ad21
Chase MacDonnell <macdonnell.chase@gmail.com>
2024-03-27 15:04:58 -0400

    Use secure cookies

--- backend/server.go
+++ backend/server.go
@@ -32,12 +32,10 @@ func main() {
 	CSECRET, csecret_exists := os.LookupEnv("SG_GITHUB_CSECRET")
 	ORIGINS := []string{"https://studygator.chasemacdonnell.net", "https://studygator-api.chasemacdonnell.net"}
 	COOKIE_DOMAIN := "chasemacdonnell.net"
-	SAMESITE := http.SameSiteNoneMode
 	if !exists {
 		SECRET = "dev-secret"
 		URL = "http://localhost:8080"
 		COOKIE_DOMAIN = "localhost"
-		SAMESITE = http.SameSiteDefaultMode
 		ORIGINS = []string{"http://localhost:5173", "http://localhost:8080", "https://studygator.chasemacdonnell.net", "https://studygator-api.chasemacdonnell.net"}
 	}
 
@@ -63,8 +61,8 @@ func main() {
 		CookieDuration:  time.Hour * 24,  // cookie expires in 1 day and will enforce re-login
 		Issuer:          "studygator",
 		URL:             URL,
-		SameSiteCookie:  SAMESITE,
 		JWTCookieDomain: COOKIE_DOMAIN,
+		SecureCookies:   true,
 		AvatarStore:     avatar.NewLocalFS("./avatars"),
 		// GraphQL by nature only uses POST requests, eliminating most XSRF vulneabilities.,
 		// Plus XSRF tokens are a quite controversial topic, and arguably dont work.
